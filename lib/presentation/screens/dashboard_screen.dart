3ff2efdf5c5c915064f59f23d1b18722277f517c
Pranav Khandelwal <pranavkhandelwal21@gmail.com>
2026-06-27 12:11:06 +0530

    Add manual battery logging feature

--- lib/presentation/screens/dashboard_screen.dart
+++ lib/presentation/screens/dashboard_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:voltwatch/data/models/battery_log_model.dart';
 import '../viewmodels/battery_provider.dart';
 import '../widgets/battery_gauge.dart';
+import '../../data/models/battery_log_model.dart';
+import '../viewmodels/log_provider.dart';
 
 /// Main dashboard screen
 class DashboardScreen extends ConsumerWidget {
@@ -13,10 +16,7 @@ class DashboardScreen extends ConsumerWidget {
     final batteryState = ref.watch(batteryStateStreamProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("VoltWatch"),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text("VoltWatch"), centerTitle: true),
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
@@ -24,15 +24,9 @@ class DashboardScreen extends ConsumerWidget {
 
           // Live battery gauge
           batteryLevel.when(
-            data: (level) => Center(
-              child: BatteryGauge(
-                batteryLevel: level,
-              ),
-            ),
-            loading: () =>
-                const Center(child: CircularProgressIndicator()),
-            error: (error, stack) =>
-                Center(child: Text(error.toString())),
+            data: (level) => Center(child: BatteryGauge(batteryLevel: level)),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stack) => Center(child: Text(error.toString())),
           ),
 
           const SizedBox(height: 40),
@@ -55,18 +49,14 @@ class DashboardScreen extends ConsumerWidget {
                     const SizedBox(height: 10),
                     Text(
                       state.name.toUpperCase(),
-                      style: const TextStyle(
-                        fontSize: 22,
-                      ),
+                      style: const TextStyle(fontSize: 22),
                     ),
                   ],
                 ),
               ),
             ),
-            loading: () =>
-                const Center(child: CircularProgressIndicator()),
-            error: (error, stack) =>
-                Center(child: Text(error.toString())),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stack) => Center(child: Text(error.toString())),
           ),
 
           const SizedBox(height: 30),
@@ -79,6 +69,39 @@ class DashboardScreen extends ConsumerWidget {
             },
             icon: const Icon(Icons.refresh),
             label: const Text("Refresh Now"),
+          ),
+
+          const SizedBox(height: 20),
+
+          ElevatedButton.icon(
+            onPressed: () async {
+              final currentLevel = await ref
+                  .read(batteryServiceProvider)
+                  .getBatteryLevel();
+
+              final currentState = await ref
+                  .read(batteryServiceProvider)
+                  .getBatteryState();
+
+              final log = BatteryLog(
+                batteryLevel: currentLevel,
+                batteryState: currentState.name,
+                timestamp: DateTime.now(),
+              );
+
+              await ref.read(batteryRepositoryProvider).saveLog(log);
+
+              // Refresh logs provider
+              ref.read(batteryLogsProvider.notifier).state = ref
+                  .read(batteryRepositoryProvider)
+                  .getLogs();
+
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Battery log saved")),
+              );
+            },
+            icon: const Icon(Icons.save),
+            label: const Text("Save Battery Log"),
           ),
         ],
       ),
--- lib/presentation/viewmodels/log_provider.dart
+++ lib/presentation/viewmodels/log_provider.dart
@@ -1,13 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import '../../data/models/battery_log_model.dart';
 import '../../data/repositories/battery_repository.dart';
 
-/// Repository provider
-final batteryRepositoryProvider = Provider((ref) {
+/// Provides BatteryRepository globally
+final batteryRepositoryProvider = Provider<BatteryRepository>((ref) {
   return BatteryRepository();
 });
 
-/// Fetch all battery logs
-final batteryLogsProvider = StateProvider<List<BatteryLog>>((ref) {
+/// Stores all battery logs in memory
+final batteryLogsProvider =
+    StateProvider<List<BatteryLog>>((ref) {
   return ref.read(batteryRepositoryProvider).getLogs();
 });
