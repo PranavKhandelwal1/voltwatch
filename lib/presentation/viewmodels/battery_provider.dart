44d9bc324a5c461eba006182eaae1a42d91a87b9
Pranav Khandelwal <pranavkhandelwal21@gmail.com>
2026-06-27 11:41:48 +0530

    Add real-time battery providers

--- lib/presentation/viewmodels/battery_provider.dart
+++ lib/presentation/viewmodels/battery_provider.dart
@@ -1,19 +1,30 @@
+import 'dart:async';
+import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/services/battery_service.dart';
 
-/// Provides BatteryService instance globally
+/// Provides BatteryService globally
 final batteryServiceProvider = Provider((ref) {
   return BatteryService();
 });
 
-/// Fetches battery level asynchronously
-final batteryLevelProvider = FutureProvider<int>((ref) async {
+/// Real-time battery level provider
+final batteryLevelStreamProvider = StreamProvider<int>((ref) async* {
   final batteryService = ref.read(batteryServiceProvider);
-  return batteryService.getBatteryLevel();
+
+  while (true) {
+    // Fetch battery percentage every 5 seconds
+    final level = await batteryService.getBatteryLevel();
+
+    yield level;
+
+    await Future.delayed(const Duration(seconds: 5));
+  }
 });
 
-/// Fetches battery state asynchronously
-final batteryStateProvider = FutureProvider((ref) async {
+/// Real-time battery state provider
+final batteryStateStreamProvider = StreamProvider<BatteryState>((ref) {
   final batteryService = ref.read(batteryServiceProvider);
-  return batteryService.getBatteryState();
+
+  return batteryService.batteryStateStream();
 });
